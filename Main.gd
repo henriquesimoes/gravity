6891fc964503c790faaac983977ccdad7a631496
Henrique Simões <henriquesimoespa@gmail.com>
2019-04-14 22:48:44 -0300

    Rebuild ground generation with TileMap

--- Main.gd
+++ Main.gd
@@ -3,26 +3,49 @@ extends "res://Scripts/minigame.gd"
 const BLOCK_SIZE = 40
 const WINDOW_HEIGHT = 400
 const WINDOW_WIDTH = 640
-const PADDING = 0
 
-var column = int(WINDOW_WIDTH/BLOCK_SIZE) + 1
-var line = int(WINDOW_HEIGHT/BLOCK_SIZE) + 1
+var column = int(WINDOW_WIDTH/BLOCK_SIZE)
+var row = int(WINDOW_HEIGHT/BLOCK_SIZE)
 
 func _ready():
-	var tilebox = load("res://Minigames/Gravity/Scripts/Tilebox.gd")
-	var tile = tilebox.new()#instance of class tilebox.gd
-	self.add_child( tile )#adding to scene
-	var path_from_class = tile.generatePath(column, line)#creating path using classes function
-	var wall = tile.create_wall(path_from_class)#creating wall using classes bluewall object
-	var velocity = 0
-	tile.add_wall_y(wall, 200)
-
+	var path = gen_path(column)
+	apply_path(path)
 	pass
 
 func minigame_start():
 	.minigame_start()
-	
 	pass
-func _process(delta):
 	
+func _process(delta):
+	fall(delta)
+	pass
+
+func apply_path(path_trace):
+	var height = $TileMap.get_used_rect().size.y
+	var width = $TileMap.get_used_rect().size.x
+	for i in range(height):
+		for j in range(width):
+			if path_trace[i].find(j) > -1:
+				$TileMap.set_cell(j, i, -1)
+	pass
+	
+func gen_path (length, depth = 100, base = randi()):
+	base %= length - int(length/2)
+	base += int(length/2)
+	var result = []
+	for i in range(depth):
+		var value = []
+		var random = randi() % 3 - 1
+		while (base + random < 0 or base + random > length - 1):
+			random = randi() % 3 - 1
+		value.append(base)
+		if random != 0:
+			value.append(base + random)
+		base += random
+		result.append(value)
+	return result
+	
+func fall(delta):
+	var multiplier = difficulty * 100
+	$TileMap.position.y -= multiplier * delta
 	pass
